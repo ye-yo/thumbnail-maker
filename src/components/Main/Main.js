5bec6f719500928cf8f5293eb6cbf556cf7fc5ce
ye-yo <yeyo0x0@gmail.com>
2022-01-24 00:30:25 +0900

    feat(ratio): Added dynamically changing the thumbnail size

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './_Main.scss';
 import LayoutForm from '../Form/LayoutForm.js';
 import BackgroundForm from '../Form/BackgroundForm.js';
@@ -8,22 +8,41 @@ import { BsDownload } from "react-icons/bs";
 
 
 function Main() {
+
+    const ratioList = [
+        { width: 400, height: 400, text: '1:1' },
+        { width: 400, height: 300, text: '4:3' },
+        {
+            width: 768, height: 402.094, text: 'Velog', subText: '(768:402.094)',
+            icon: <img src='https://media.vlpt.us/images/velog/profile/9aa07f66-5fcd-41f4-84f2-91d73afcec28/green%20favicon.png?w=240)' />
+        },
+        {
+            width: 400, height: 225, text: 'Youtube', subText: '(1280:720)',
+            outputWidth: 1280, outputHeight: 720,
+            icon: <svg viewBox="0 0 28 20" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon"><g viewBox="0 0 90 20" preserveAspectRatio="xMidYMid meet" class="style-scope yt-icon"><g class="style-scope yt-icon"><path d="M27.9727 3.12324C27.6435 1.89323 26.6768 0.926623 25.4468 0.597366C23.2197 2.24288e-07 14.285 0 14.285 0C14.285 0 5.35042 2.24288e-07 3.12323 0.597366C1.89323 0.926623 0.926623 1.89323 0.597366 3.12324C2.24288e-07 5.35042 0 10 0 10C0 10 2.24288e-07 14.6496 0.597366 16.8768C0.926623 18.1068 1.89323 19.0734 3.12323 19.4026C5.35042 20 14.285 20 14.285 20C14.285 20 23.2197 20 25.4468 19.4026C26.6768 19.0734 27.6435 18.1068 27.9727 16.8768C28.5701 14.6496 28.5701 10 28.5701 10C28.5701 10 28.5677 5.35042 27.9727 3.12324Z" fill="#FF0000" class="style-scope yt-icon"></path><path d="M11.4253 14.2854L18.8477 10.0004L11.4253 5.71533V14.2854Z" fill="white" class="style-scope yt-icon"></path></g></g></svg>
+        },
+    ]
+
+    const [ratio, setRatio] = useState(ratioList[2]);
+    const [layout, setLayout] = useState(null);
+    const [background, setBackground] = useState(null);
+    const [assets, setAssets] = useState(null);
     return (
         <main>
             <div className="center-box">
                 <section className="section-canvas">
                     <div className="input-canvas-size-wrap">
-                        <input type="number" value="400" />X
-                        <input type="number" value="300" />
+                        <input type="number" value={ratio.width} />X
+                        <input type="number" value={ratio.height} />
                     </div>
                     <div className="canvas-box">
-                        <div className="canvas" style={{ width: 400, height: 300 }}></div>
+                        <div className="canvas" style={{ width: ratio.width, height: ratio.height }}></div>
                     </div>
                     <button className="btn-download btn-main"><BsDownload />Download</button>
                 </section>
                 <section className="section-form">
-                    <RatioForm />
-                    <LayoutForm />
+                    <RatioForm setRatio={setRatio} ratioList={ratioList} />
+                    <LayoutForm setLayout={setLayout} />
                     <BackgroundForm />
                     <AssetsForm />
                 </section>
